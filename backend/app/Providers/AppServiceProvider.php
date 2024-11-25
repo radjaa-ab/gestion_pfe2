54ca7fa041bec73ab782179074a1998e974b730b
ikramnadiaboudghenestambouli <ikramboudghenestambouli835@gmail.com>
2024-11-25 19:18:08 +0100

    supprimet hadak backend w 3wdt installet laravel jdid kano fih swlh naessin

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,8 +17,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        //
     }
 }
